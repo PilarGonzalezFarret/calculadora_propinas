9be8ec8b6f268389896781b53b1b904bc5ea291d
Pilar González-Farret <pilar.gonzalezfarret@gmail.com>
2025-06-07 17:49:40 -0400

    update

--- calculadora_propinas/src/App.tsx
+++ calculadora_propinas/src/App.tsx
@@ -1,35 +1,38 @@
-import MenuItem from "./components/MenuItem"
-import { menuItems } from "./data/db"
+import MenuItem from "./components/MenuItem";
+import { menuItems } from "./data/db";
+import useOrder from "./hooks/useOrder";
 
 
 function App() {
-
+  const{ addItem } = useOrder()
   return (
-  <>
-    <header className='bg-teal-400 py-5'>
-      <h1 className='text-center text-4xl font-black'>Calculadora de Propinas y Consumo</h1>
-    </header>
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">
+          Calculadora de Propinas y Consumo
+        </h1>
+      </header>
 
-    <main className="max-w-7xl mx-auto pt-20 grid md:grid-cols-2">
-      <div>
-        <h2>Menú</h2>
-        
-          {menuItems.map(item => (
-          <MenuItem
-          key={item.id}
+      <main className="max-w-7xl mx-auto pt-20 grid md:grid-cols-2">
+        <div p-5>
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem 
+              key={item.id} 
+              item={item} 
+              addItem={addItem}
+              />
+            ))}
+          </div>
+        </div>
 
-          />
-        ))}
-      </div>
-      
-      <div>
-        <h2>Consumo</h2>
-      </div>
-      
-    </main>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
